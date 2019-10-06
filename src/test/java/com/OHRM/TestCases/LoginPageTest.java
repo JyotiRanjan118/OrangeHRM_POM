c71c05cf3a03d989d630ed3ba0ad7c755ecd4573
JyotiRanjan118 <jyotiranjan118@gmail.com>
2019-10-06 22:46:42 +0530

    Modified project with Maven

--- src/test/java/com/OHRM/TestCases/LoginPageTest.java
+++ src/test/java/com/OHRM/TestCases/LoginPageTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.ExtentReportPOM.ExtentReport_OHRM;
+
 import com.OHRM.Pages.HomePage;
 import com.OHRM.Pages.LoginPage;
 import com.OHRM.TestBase.TestBase;
@@ -17,16 +17,13 @@ import com.OHRM.TestBase.TestBase;
 public class LoginPageTest extends TestBase {
 	LoginPage loginPage;
 	HomePage homePage;
-	ExtentReport_OHRM extent_report;
+	
 	
 	public LoginPageTest() throws IOException {
 		super();
 	}
 	
-	@BeforeTest
-	public void extentreport() {
-	extent_report.setUPExtent();
-	}
+	
 	@BeforeMethod
 	public void setup() throws IOException {
 		
@@ -37,7 +34,7 @@ public class LoginPageTest extends TestBase {
 	
 	@Test(priority=1)
 	public void LoginPageTitleTest() {
-		extent_report.test("LoginPageTitleTest");
+		
 		String Title=loginPage.validateLoginPageTitle();
 		Assert.assertEquals(Title, "OrangeHRM");
 		
@@ -45,14 +42,14 @@ public class LoginPageTest extends TestBase {
 	
 	@Test(priority=2)
 	public void validateLogoTest() {
-		extent_report.test("validateLogoTest");
+		
 		Boolean flag=loginPage.validateLogoOHRM();
 		Assert.assertTrue(flag);
 	}
 	
 	@Test(priority=3)
 	public void LoginTest() throws IOException {
-		extent_report.test("LoginTest");
+		
 		homePage=loginPage.login(prop.getProperty("UserName"), prop.getProperty("PassWord"));
 		
 	}
@@ -64,11 +61,6 @@ public class LoginPageTest extends TestBase {
 		
 	}
 	
-	@AfterTest
-	public void tearReport()
-	{
-		extent_report.endreport();
-	}
 	
 	
 }
